2e612e287f0f9578c943fe6f06d43643a8378b67
David Nelson <eatdrinksleepcode@gmail.com>
2015-08-25 16:00:05 -0500

    Execute Boo script from Casper

---
+++ Casper.Console.Test/ConsoleTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using System.IO;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Casper.Console.Test {
+	[TestFixture]
+	public class ConsoleTests {
+		[Test]
+		public void ExecuteScript() {
+			var testProcess = ExecuteScript("Test1.casper", "print 'Hello World!'");
+			Assert.That(testProcess.StandardError.ReadToEnd(), Is.Empty);
+			Assert.That(testProcess.ExitCode, Is.EqualTo(0));
+			Assert.That(testProcess.StandardOutput.ReadLine(), Is.EqualTo("Hello World!"));
+		}
+
+		[Test]
+		public void CompilationFailure() {
+			var testProcess = ExecuteScript("Test1.casper", "foobar");
+			Assert.That(testProcess.StandardError.ReadToEnd(), Is.Not.Empty);
+			Assert.That(testProcess.ExitCode, Is.EqualTo(1));
+			Assert.That(testProcess.StandardOutput.ReadLine(), Is.Null);
+		}
+
+		[Test]
+		public void UnhandledException() {
+			var testProcess = ExecuteScript("Test1.casper", @"raise System.Exception(""Script failure"")");
+			Assert.That(testProcess.StandardError.ReadLine(), Is.EqualTo("System.Exception: Script failure"));
+			Assert.That(testProcess.ExitCode, Is.EqualTo(255));
+			Assert.That(testProcess.StandardOutput.ReadLine(), Is.Null);
+		}
+
+		static Process ExecuteScript(string scriptName, string scriptContents) {
+			Process testProcess;
+			try {
+				File.WriteAllText(scriptName, scriptContents);
+				testProcess = Process.Start(new ProcessStartInfo {
+					FileName = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "casper.exe"),
+					Arguments = scriptName,
+					UseShellExecute = false,
+					RedirectStandardOutput = true,
+					RedirectStandardError = true,
+				});
+				testProcess.WaitForExit();
+			}
+			finally {
+				File.Delete(scriptName);
+			}
+			return testProcess;
+		}
+	}
+}
+
---
+++ Casper.Console/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Boo.Lang.Compiler;
+using Boo.Lang.Compiler.Pipelines;
+using Boo.Lang.Compiler.IO;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
+
+namespace Casper {
+	public static class MainClass {
+		const int EXIT_CODE_COMPILATION_ERROR = 1;
+		const int EXIT_CODE_UNHANDLED_EXCEPTION = 255;
+		
+		public static int Main(string[] args) {
+			AppDomain.CurrentDomain.UnhandledException += (sender, e) => {
+				Console.Error.WriteLine(e.ExceptionObject);
+				Environment.Exit(EXIT_CODE_UNHANDLED_EXCEPTION);
+			};
+
+			var compileParams = new CompilerParameters();
+			compileParams.GenerateInMemory = true;
+			compileParams.Input.Add(new FileInput(args[0]));
+			var context = new CompilerContext(compileParams);
+			new CompileToMemory().Run(context);
+
+			if (context.Errors.Count > 0) {
+				Console.Error.WriteLine(context.Errors);
+				return EXIT_CODE_COMPILATION_ERROR;
+			}
+
+			try {
+				context.GeneratedAssembly.EntryPoint.Invoke(null, new Object[] { new String[0] });
+			} catch(TargetInvocationException ex) {
+				ExceptionDispatchInfo.Capture(ex.InnerException).Throw();
+			}
+
+			return 0;
+		}
+	}
+}
