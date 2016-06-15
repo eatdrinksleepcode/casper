4f484b7cd7a5a4fc347540e3397e54cdc4c57baf
David Nelson <eatdrinksleepcode@gmail.com>
2016-06-15 00:46:52 -0500

    Split out ConfigureSolution tests

--- Test.Integration/SolutionTests.cs
+++ Test.Integration/SolutionTests.cs
@@ -26,7 +26,7 @@ namespace Casper {
 		}
 		
 		[Test]
-		public void ConfigureSolution() {
+		public void ConfigureAllProjectsWithDependencies() {
 			var slnFile = projectRootPath.File("Test.sln");
 			slnFile.WriteAllText(@"
 Microsoft Visual Studio Solution File, Format Version 12.00
@@ -34,12 +34,6 @@ Microsoft Visual Studio Solution File, Format Version 12.00
 VisualStudioVersion = 12.0.31101.0
 MinimumVisualStudioVersion = 10.0.40219.1
 Project(""{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}"") = ""My.Library"", ""Library\My.Library.csproj"", ""{1027A646-4C10-44B9-939F-639D49D7CBF7}""
-EndProject
-Project(""{2150E333-8FDC-42A3-9474-1A3956D46DE8}"") = ""Extra Files"", ""Extras"", ""{42CCF79A-CE5F-4FB8-ABD4-F7E747823504}""
-	ProjectSection(SolutionItems) = preProject
-		Extras\AssemblyInfo.cs = Extras\AssemblyInfo.cs
-		Extras\packages.config = Extras\packages.config
-	EndProjectSection
 EndProject
 Project(""{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}"") = ""My.Library.UnitTests"", ""Library.UnitTests\My.Library.UnitTests.csproj"", ""{3D7D7E4E-5F2B-44DD-9AB8-B52468ECFC41}""
 EndProject
@@ -63,13 +57,70 @@ EndProject
 );
 
 			var rootProject = new TestProject(fileSystem, projectRootPath);
-			var libraryProject = new TestProject(rootProject, projectRootPath.Directory("Library"), "My.Library");
-			var libraryTestProject = new TestProject(rootProject, projectRootPath.Directory("Library.UnitTests"), "My.Library.UnitTests");
 			rootProject.ConfigureFromSolution(slnFile.Path);
 
 			Assert.That(rootProject.Projects.Count, Is.EqualTo(2));
+			var libraryProject = rootProject.Projects["My.Library"];
+			var libraryTestProject = rootProject.Projects["My.Library.UnitTests"];
 			Assert.That(libraryTestProject.Tasks["Compile"].DependsOn, Contains.Item(libraryProject.Tasks["Compile"]));
 			CollectionAssert.IsEmpty(libraryTestProject.Tasks["Clean"].DependsOn);
+		}
+
+		[Test]
+		public void ConfigureProjectFromSolutionAlreadyIncluded() {
+			var slnFile = projectRootPath.File("Test.sln");
+			slnFile.WriteAllText(@"
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio 2013
+VisualStudioVersion = 12.0.31101.0
+MinimumVisualStudioVersion = 10.0.40219.1
+Project(""{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}"") = ""My.Library"", ""Library\My.Library.csproj"", ""{1027A646-4C10-44B9-939F-639D49D7CBF7}""
+EndProject
+");
+
+			projectRootPath.Directory("Library").File("My.Library.csproj").WriteAllText(
+@"<?xml version=""1.0"" encoding=""utf-8""?>
+<Project xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
+</Project>
+");
+
+			var rootProject = new TestProject(fileSystem, projectRootPath);
+			var libraryProject = new TestProject(rootProject, projectRootPath.Directory("Library"), "My.Library");
+			rootProject.ConfigureFromSolution(slnFile.Path);
+			libraryProject.AddTask("Task1", new TestTask());
+
+			Assert.That(rootProject.Projects.Count, Is.EqualTo(1));
+			Assert.That(libraryProject.Tasks, Has.Count.EqualTo(3));
+		}
+
+		[Test]
+		public void ConfigureSolutionWithSolutionDirectory() {
+			var slnFile = projectRootPath.File("Test.sln");
+			slnFile.WriteAllText(@"
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio 2013
+VisualStudioVersion = 12.0.31101.0
+MinimumVisualStudioVersion = 10.0.40219.1
+Project(""{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}"") = ""My.Library"", ""Library\My.Library.csproj"", ""{1027A646-4C10-44B9-939F-639D49D7CBF7}""
+EndProject
+Project(""{2150E333-8FDC-42A3-9474-1A3956D46DE8}"") = ""Extra Files"", ""Extras"", ""{42CCF79A-CE5F-4FB8-ABD4-F7E747823504}""
+	ProjectSection(SolutionItems) = preProject
+		Extras\AssemblyInfo.cs = Extras\AssemblyInfo.cs
+		Extras\packages.config = Extras\packages.config
+	EndProjectSection
+EndProject
+");
+
+			projectRootPath.Directory("Library").File("My.Library.csproj").WriteAllText(
+@"<?xml version=""1.0"" encoding=""utf-8""?>
+<Project xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
+</Project>
+");
+
+			var rootProject = new TestProject(fileSystem, projectRootPath);
+			rootProject.ConfigureFromSolution(slnFile.Path);
+
+			Assert.That(rootProject.Projects.Count, Is.EqualTo(1));
 		}
 	}
 }
