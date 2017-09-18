b28ee6d46b86c97712b0c4b4d933b63a381d3e3a
David Nelson <eatdrinksleepcode@gmail.com>
2017-09-17 23:08:49 -0500

    Update NuGet

--- dogfood.cmd
+++ dogfood.cmd
@@ -10,8 +10,6 @@ mkdir "%myDir%.msbuild" || goto :eof
 echo Installing casper...
 "%myDir%nuget" sources Remove -Name "Casper Dev" -ConfigFile "%localNugetConfig%" >NUL 2>&1
 "%myDir%nuget" sources Add -Name "Casper Dev" -Source "%myDir%out\packages" -ConfigFile "%localNugetConfig%" >NUL || goto :eof
-REM Adding any source to a NuGet config also adds the default source, which we don't want for this operation
-"%myDir%nuget" sources Disable -Name "https://www.nuget.org/api/v2/" -ConfigFile "%localNugetConfig%" >NUL || goto :eof
 "%myDir%nuget" install Casper.Console -OutputDirectory "%myDir%.msbuild" -pre -Verbosity quiet -Source "Casper Dev" -ConfigFile "%localNugetConfig%" -ExcludeVersion || goto :eof
 del "%localNugetConfig%"
 echo Cleaning outputs...
