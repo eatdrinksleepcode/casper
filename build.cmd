e52f10305c3e5f62f27f5ca5a898823d432f826d
David Nelson <eatdrinksleepcode@gmail.com>
2017-12-27 23:15:44 -0600

    Generate version with GitVersion

--- build.cmd
+++ build.cmd
@@ -4,6 +4,9 @@ set myDir=%~dp0
 set configuration=%1
 if "%configuration%" == "" set configuration=RELEASE
 
+echo Determining version...
+packages\GitVersion.CommandLine.3.6.5\tools\GitVersion.exe /updateAssemblyInfo AssemblyInfo.Version.cs /ensureAssemblyInfo || goto :eof
+
 set TargetFrameworkVersion=v4.5.1
 echo Building with msbuild...
 msbuild /p:Configuration=%configuration% /p:TargetFrameworkversion=v4.5.1 /verbosity:quiet || goto :eof
