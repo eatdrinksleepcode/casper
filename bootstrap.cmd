7b86b172db45b9fb59767d96a97b80fda76fc6ef
David Nelson <eatdrinksleepcode@gmail.com>
2018-04-24 23:28:15 -0500

    Move most of the dogfood process into a Casper build script

---
+++ bootstrap.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+set myDir=%~dp0
+set localNugetConfig=%myDir%local-nuget.config
+
+echo Installing old Casper...
+set oldPackageDir=%myDir%.casper\old
+if exist "%oldPackageDir%" rmdir /S /Q "%oldPackageDir%" || goto :eof
+mkdir "%oldPackageDir%" || goto :eof
+"%myDir%nuget" install Casper.Console -Version 0.1.0-ci0146 -OutputDirectory "%oldPackageDir%" -pre -Verbosity quiet -Source "MyGet" -ConfigFile "%localNugetConfig%" -ExcludeVersion || goto :eof
