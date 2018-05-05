3169305e7433d335679f9e926e4f12462f1be49c
David Nelson <eatdrinksleepcode@gmail.com>
2018-05-04 22:27:58 -0500

    Upgrade Casper version for build

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -7,4 +7,4 @@ echo Installing old Casper...
 set oldPackageDir=%myDir%.casper\old
 if exist "%oldPackageDir%" rmdir /S /Q "%oldPackageDir%" || goto :eof
 mkdir "%oldPackageDir%" || goto :eof
-"%myDir%nuget" install Casper.Console -Version 0.1.0-ci0146 -OutputDirectory "%oldPackageDir%" -pre -Verbosity quiet -Source "MyGet" -ConfigFile "%localNugetConfig%" -ExcludeVersion || goto :eof
+"%myDir%nuget" install Casper.Console -Version 0.1.0-ci0162 -OutputDirectory "%oldPackageDir%" -pre -Verbosity quiet -Source "MyGet" -ConfigFile "%localNugetConfig%" -ExcludeVersion || goto :eof
